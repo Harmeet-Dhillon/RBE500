580e559c9609866a54873642dfb459f341911b52
Harmeet <hdhillon3196820@icloud.com>
2024-12-24 00:15:07 -0500

    teleop node

--- cmd_node.cpp
+++ cmd_node.cpp
@@ -1,35 +1,65 @@
-#include <memory>
+#include <ros/ros.h>
+#include "std_msgs/String.h"
 
-#include "rclcpp/rclcpp.hpp"
-#include "std_msgs/msg/string.hpp"
-#include "std_msgs/msg/int32.hpp"
-using std::placeholders::_1;
-
-class MySubscriber : public rclcpp::Node
+class Cmd_Node
 {
-  public:
-    MySubscriber()
-    : Node("my_subscriber")
+public:
+    Cmd_Node()
     {
-      subscription_ = this->create_subscription<std_msgs::msg::Int32>(
-      "topic", 10, std::bind(&MySubscriber::topic_callback, this, _1));
+        // Initialize the node handle
+        nh_ = ros::NodeHandle();
+
+        // Create a subscriber that listens to the "jetson_commands" topic
+        subscription_ = nh_.subscribe("jetson_commands", 1, &Cmd_Node::topicCallback, this); 
+        // Publisher to send messages (if required, initialize publisher as needed)
+        publisher_ = nh_.advertise<std_msgs::String>("cmd_response", 10); 
     }
 
-  private:
-    void topic_callback(const std_msgs::msg::Int32 & msg) const
+    // Callback function to process received messages
+    void topicCallback(const std_msgs::String::ConstPtr& msg)
     {
-     
-      if(msg.data % 2==0){
-       RCLCPP_INFO(this->get_logger(), "I received: '%d'.It is an even number",msg.data);
+        std_msgs::String new_msg;
+
+        // Handling the command strings and setting new messages
+        if (msg->data == "high") {
+            new_msg.data = "High command received!";
+        }
+        else if (msg->data == "low") {
+            new_msg.data = "Low command received!";
+        }
+        else if (msg->data == "left") {
+            new_msg.data = "Turn left!";
+        }    
+        else if (msg->data == "right") {
+            new_msg.data = "Turn right!";
+        }  
+        else if (msg->data == "back") {
+            new_msg.data = "Move back!";
+        }  
+        else if (msg->data == "stop") {
+            new_msg.data = "Stop command received!";
+        }   
+        else {
+            new_msg.data = "Invalid command!";
+        }
+
+        // Publish the new message to a topic if required
+        publisher_.publish(new_msg);
     }
-    else{RCLCPP_INFO(this->get_logger(), "I received: '%d'.It is an odd number",msg.data);}}
-    rclcpp::Subscription<std_msgs::msg::Int32>::SharedPtr subscription_;
+
+private:
+    ros::NodeHandle nh_;  // Node handle
+    ros::Subscriber subscription_;  // Subscriber object
+    ros::Publisher publisher_;  // Publisher object
 };
 
-int main(int argc, char * argv[])
+int main(int argc, char** argv)
 {
-  rclcpp::init(argc, argv);
-  rclcpp::spin(std::make_shared<MySubscriber>());
-  rclcpp::shutdown();
-  return 0;
+    ros::init(argc, argv, "cmd_node");  // Initialize the ROS node
+    Cmd_Node cmd_node;  // Create an instance of the Cmd_Node class
+
+    ros::spin();  // Keep the program alive to receive messages
+
+    return 0;
 }
+
